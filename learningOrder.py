4277178b70a3bffd69074e01be5cfe6c547b78fd
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2025-04-21 16:41:26 +0500

    successfully posted the scraped data to cloud elastic seach and now pushing the data to local docker image

---
+++ learningOrder.py
@@ -0,0 +1,12 @@
+# 📚 Suggested Learning Order:
+# What is Elasticsearch?
+
+# Cluster, Node, Index, Document, Field
+
+# CRUD operations
+
+# Search basics (Match, Term, Bool)
+
+# Filters vs Queries
+
+# Simple Aggregations
